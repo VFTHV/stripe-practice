586a02a2627426f6238943920d4dca7b3c86a14d
Vadim Fattakhov <vadfthv@gmail.com>
2024-03-28 13:59:00 -0500

    refactor stripeController

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,6 +1,20 @@
-const stripe = async (req, res) => {
-  console.log(req.body);
-  res.send('stripe route');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const stripeController = async (req, res) => {
+  const { purchase, total_amount, shipping_fee } = req.body;
+
+  const calculateOrderAmount = () => {
+    // here you need to check price of each item against values in database
+    // and eventually calculate total amount
+    return total_amount + shipping_fee;
+  };
+
+  const paymentIntent = await stripe.paymentIntent.create({
+    amount: calculateOrderAmount(),
+    currency: 'usd',
+  });
+  console.log(paymentIntent);
+  res.json({ clientSecret: paymentIntent.client_secret });
 };
 
-module.exports = stripe;
+module.exports = stripeController;
